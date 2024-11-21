617f2eb4baaeba075fbcfb8cecb6758e395a32b6
AdreshSingh <adreshsingh271527@gmail.com>
2024-11-21 08:36:53 +0530

    modified for own site like side bar and title of the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,46 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'All4One',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Learn HTML',
+                items: [
+                    {
+                        label: 'HTML Basic', slug: 'html/basic'
+                    },
+                    {
+                        label: 'HTML Component', slug: 'html/component'
+                    },
+                    {
+                        label: 'HTML Attributes', slug: 'html/attributes'
+                    },
+                    {
+                        label: 'Presentational Tags', slug: 'html/presentational'
+                    },
+                ]
+            },
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    }), react(), tailwind()],
 });
